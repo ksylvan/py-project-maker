f7bea9cb54f2a00ac40ce679aedd10a0e843e15f
Kayvan Sylvan <kayvan@sylvan.com>
2025-05-10 20:46:23 -0700

    chore: fix line too long linter error

--- src/pyhatchery/main.py
+++ src/pyhatchery/main.py
@@ -7,7 +7,11 @@ from pyhatchery import __version__
 
 def main():
     """Main function to execute the script."""
-    click.echo(f"Hello from pyhatchery! Version {click.style(__version__, fg='green', bold=True)}.")
+    click.echo(
+        f"Hello from pyhatchery! Version {
+            click.style(__version__, fg='green', bold=True)
+        }.",
+    )
 
 
 if __name__ == "__main__":
